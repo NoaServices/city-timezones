398b25de5e3786584f4a57ac6a74422282fe626b
Delphin Rako <delphin@meetsally.ai>
2017-10-04 14:06:31 +0300

    add some methodes

--- index.js
+++ index.js
@@ -1,16 +1,93 @@
 "use strict";
-const _ = require('lodash')
-const cityMapping = require('./data/cityMap.json')
+const _ = require('lodash');
+const cities = require('./data/cityMap.json');
 
-function lookupViaCity(city) {
-  const cityLookup = _.filter(cityMapping, function (o) { return o.city === city })
-  if (cityLookup && cityLookup.length > 0) {
-    return cityLookup
-  } else {
-    return []
+/**
+ * Launch the app as a child process to able to restart it
+ */
+module.exports = class CityTimezones {
+
+  /**
+   * Find the 1st city in zone of a timezone
+   *
+   * @param {String} timezone
+   * @returns {String}
+   */
+  getCity(timezone) {
+    if (!timezone) return '';
+    const place = timezone.split('/').pop().replace('_', ' ');
+    const filteredCityTimezones = _.filter(cities, { timezone });
+    let zone = _.find(filteredCityTimezones, cityTimezone => {
+      return (cityTimezone.city === place || cityTimezone.country === place) && cityTimezone.timezone === timezone;
+    });
+    if (!zone) zone = filteredCityTimezones[0];
+    return zone ? `${zone.city}, ${zone.country}` : timezone;
   }
-}
 
-module.exports = {
-  lookupViaCity
+  /**
+   * Find timezone of a city
+   *
+   * @param {String} value
+   * @returns {String}
+   */
+  getTimezone(value) {
+    if (!value) return '';
+    let [city, country] = value.split(',');
+    let zone;
+    city = city.trim().toLowerCase();
+
+    if (country) {
+      country = country.trim().toLowerCase();
+      zone = this._findByCityAndCountry(city, country);
+      return zone.timezone;
+    }
+
+    zone = this._findByCity(city) || this._findByCountry(city);
+    if (!zone) return '';
+    return zone.timezone;
+  }
+
+  /**
+   * Get the list of cities which have a timezone
+   * @returns {Array}
+   */
+  getData() {
+    return cities;
+  }
+
+  /**
+   * Find by city
+   *
+   * @param {String} city
+   * @private
+   */
+  _findByCity(city) {
+    return _.find(cities, item => item.city.toLowerCase() === city);
+  }
+
+  /**
+   * Find by country
+   *
+   * @param {String} country
+   * @private
+   */
+  _findByCountry(country) {
+    return _.find(cities, item => {
+      const city = item.timezone.split('/').pop().replace('_', ' ').toLowerCase();
+      return item.city.toLowerCase() === city && item.country.toLowerCase() === country;
+    });
+  }
+
+  /**
+   * Find a city with country
+   *
+   * @param {String} city
+   * @param {String} country
+   * @private
+   */
+  _findByCityAndCountry(city, country) {
+    return _.find(cities, item => {
+      return item.city.toLowerCase() === city && item.country.toLowerCase() === country;
+    });
+  }
 };
